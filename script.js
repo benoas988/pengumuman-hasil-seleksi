d25c6d3be40d5dcc47ecf672d88fc1933f4ec226
BenoAbriyan <128135053+benoas988@users.noreply.github.com>
2023-08-23 01:27:22 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,245 @@
+var cari = document.getElementById("tombol");
+var popup = document.getElementById("popUp");
+var nim = document.getElementById("search");
+
+var hasil = document.getElementById("hasil");
+
+
+
+
+cari.addEventListener("click", function(){
+    var teks = nim.value;
+    
+    //nadra
+    if(teks=="162112333028"){
+        var a=1;
+        var anjai = "<p>Kandidat Atas Nama Nadra Hafiza "+ teks +" Selamat Anda dinyatakan Lolos pada Divisi Adminitration & Organizer!, Silakan Menunggu Informasi Pelaksanaan Magang</p>";
+        hasil.innerHTML = anjai;
+    }
+    //salsa
+    else if(teks=="162112333017"){
+        var a=1;
+        var anjai = "<p>Kandidat Atas Nama Salsabila Al 'Ismail "+ teks +" Selamat Anda dinyatakan Lolos pada Divisi Adminitration & Organizer!, Silakan Menunggu Informasi Pelaksanaan Magang</p>";
+        hasil.innerHTML = anjai;
+    }
+    //farah
+    else if(teks=="162221098"){
+        var a=1;
+        var anjai = "<p>Kandidat Atas Nama Farah Sitti Rauda Makalalag  "+ teks +" Selamat Anda dinyatakan Lolos pada Divisi Adminitration & Organizer!, Silakan Menunggu Informasi Pelaksanaan Magang</p>";
+        hasil.innerHTML = anjai;
+    }
+    //catur
+    else if(teks=="162112433016"){
+        var a=1;
+        var anjai = "<p>Kandidat Atas Nama Catur Fitria Lestari "+ teks +" Selamat Anda dinyatakan Lolos pada Divisi Finance & Sponsorship!, Silakan Menunggu Informasi Pelaksanaan Magang</p>";
+        hasil.innerHTML = anjai;
+    }
+    //angelline
+    else if(teks=="166221007"){
+        var a=1;
+        var anjai = "<p>Kandidat Atas Nama Angelline Maharani "+ teks +" Selamat Anda dinyatakan Lolos pada Divisi Finance & Sponsorship!, Silakan Menunggu Informasi Pelaksanaan Magang</p>";
+        hasil.innerHTML = anjai;
+    }
+    //dessy
+    else if(teks=="166221082"){
+        var a=1;
+        var anjai = "<p>Kandidat Atas Nama Dessy Dwi Anggraini "+ teks +" Selamat Anda dinyatakan Lolos pada Divisi Finance & Sponsorship!, Silakan Menunggu Informasi Pelaksanaan Magang</p>";
+        hasil.innerHTML = anjai;
+    }
+    //reyhan arya
+    else if(teks=="162112433077"){
+        var a=1;
+        var anjai = "<p>Kandidat Atas Nama Mohamad Reyhan Arya Yudanto Kurniawan "+ teks +" Selamat Anda dinyatakan Lolos pada Divisi Finance & Sponsorship!, Silakan Menunggu Informasi Pelaksanaan Magang</p>";
+        hasil.innerHTML = anjai;
+    }
+    //tito
+    else if(teks=="166221046"){
+        var a=1;
+        var anjai = "<p>Kandidat Atas Nama Traju Muktito Ajie "+ teks +" Selamat Anda dinyatakan Lolos pada Divisi Creative Media!, Silakan Menunggu Informasi Pelaksanaan Magang</p>";
+        hasil.innerHTML = anjai;
+    }
+    //puma
+    else if(teks=="162112533092"){
+        var a=1;
+        var anjai = "<p>Kandidat Atas Nama Putri Maharani Haryanto "+ teks +" Selamat Anda dinyatakan Lolos pada Divisi Creative Media!, Silakan Menunggu Informasi Pelaksanaan Magang</p>";
+        hasil.innerHTML = anjai;
+    }
+    //lintang
+    else if(teks=="162112233088"){
+        var a=1;
+        var anjai = "<p>Kandidat Atas Nama Lintang Syahla' Frilia "+ teks +" Selamat Anda dinyatakan Lolos pada Divisi Creative Media!, Silakan Menunggu Informasi Pelaksanaan Magang</p>";
+        hasil.innerHTML = anjai;
+    }
+    //yudha
+    else if(teks=="162112433067"){
+        var a=1;
+        var anjai = "<p>Kandidat Atas Nama Yudha Anandha Putra "+ teks +" Selamat Anda dinyatakan Lolos pada Divisi Procurement!, Silakan Menunggu Informasi Pelaksanaan Magang</p>";
+        hasil.innerHTML = anjai;
+    }
+    //mikhael
+    else if(teks=="165221036"){
+        var a=1;
+        var anjai = "<p>Kandidat Atas Nama Mikhael Dananda Agusta "+ teks +" Selamat Anda dinyatakan Lolos pada Divisi Procurement!, Silakan Menunggu Informasi Pelaksanaan Magang</p>";
+        hasil.innerHTML = anjai;
+    }
+    //fadlan
+    else if(teks=="162112533072"){
+        var a=1;
+        var anjai = "<p>Kandidat Atas Nama Fadlan Farid "+ teks +" Selamat Anda dinyatakan Lolos pada Divisi Procurement!, Silakan Menunggu Informasi Pelaksanaan Magang</p>";
+        hasil.innerHTML = anjai;
+    }
+    //devin
+    else if(teks=="166221009"){
+        var a=1;
+        var anjai = "<p>Kandidat Atas Nama Devin Nanda Restu Putra "+ teks +" Selamat Anda dinyatakan Lolos pada Divisi Controller!, Silakan Menunggu Informasi Pelaksanaan Magang</p>";
+        hasil.innerHTML = anjai;
+    }
+    //deanisa
+    else if(teks=="162112433011"){
+        var a=1;
+        var anjai = "<p>Kandidat Atas Nama DEANISA ALIYAH SUBIYANTORO "+ teks +" Selamat Anda dinyatakan Lolos pada Divisi Controller!, Silakan Menunggu Informasi Pelaksanaan Magang</p>";
+        hasil.innerHTML = anjai;
+    }
+    //fajrul
+    else if(teks=="166221054"){
+        var a=1;
+        var anjai = "<p>Kandidat Atas Nama Fajrul Ihsan Rahmat R. "+ teks +" Selamat Anda dinyatakan Lolos pada Divisi Drivetrain!, Silakan Menunggu Informasi Pelaksanaan Magang</p>";
+        hasil.innerHTML = anjai;
+    }
+    //gress
+    else if(teks=="162112433063"){
+        var a=1;
+        var anjai = "<p>Kandidat Atas Nama Gress Timur Pahlawan "+ teks +" Selamat Anda dinyatakan Lolos pada Divisi Drivetrain!, Silakan Menunggu Informasi Pelaksanaan Magang</p>";
+        hasil.innerHTML = anjai;
+    }
+    //julius
+    else if(teks=="162112533080"){
+        var a=1;
+        var anjai = "<p>Kandidat Atas Nama Julius Sucitro Hartanto "+ teks +" Selamat Anda dinyatakan Lolos pada Divisi Drivetrain!, Silakan Menunggu Informasi Pelaksanaan Magang</p>";
+        hasil.innerHTML = anjai;
+    }
+    //hanif
+    else if(teks=="162112333073"){
+        var a=1;
+        var anjai = "<p>Kandidat Atas Nama Muhammad Haniif "+ teks +" Selamat Anda dinyatakan Lolos pada Divisi Drivetrain!, Silakan Menunggu Informasi Pelaksanaan Magang</p>";
+        hasil.innerHTML = anjai;
+    }
+    //nevandra
+    else if(teks=="166221026"){
+        var a=1;
+        var anjai = "<p>Kandidat Atas Nama Muhammad Nevandra Fithra Pangestu "+ teks +" Selamat Anda dinyatakan Lolos pada Divisi Electrical!, Silakan Menunggu Informasi Pelaksanaan Magang</p>";
+        hasil.innerHTML = anjai;
+    }
+    //fikri
+    else if(teks=="162112433042"){
+        var a=1;
+        var anjai = "<p>Kandidat Atas Nama Ahmad Ahsanul Fikri "+ teks +" Selamat Anda dinyatakan Lolos pada Divisi Electrical!, Silakan Menunggu Informasi Pelaksanaan Magang</p>";
+        hasil.innerHTML = anjai;
+    }
+    //fadhil
+    else if(teks=="162112433040"){
+        var a=1;
+        var anjai = "<p>Kandidat Atas Nama FADHIL BINTANG PRAWIRA "+ teks +" Selamat Anda dinyatakan Lolos pada Divisi Electrical!, Silakan Menunggu Informasi Pelaksanaan Magang</p>";
+        hasil.innerHTML = anjai;
+    }
+    //iman
+    else if(teks=="166221014"){
+        var a=1;
+        var anjai = "<p>Kandidat Atas Nama Imanuddin Wahyu Saputra "+ teks +" Selamat Anda dinyatakan Lolos pada Divisi Electrical!, Silakan Menunggu Informasi Pelaksanaan Magang</p>";
+        hasil.innerHTML = anjai;
+    }
+    //rifqi
+    else if(teks=="166221027"){
+        var a=1;
+        var anjai = "<p>Kandidat Atas Nama Rifqi Bagus Estiawan "+ teks +" Selamat Anda dinyatakan Lolos pada Divisi Fabrikasi!, Silakan Menunggu Informasi Pelaksanaan Magang</p>";
+        hasil.innerHTML = anjai;
+    }
+    //bintang
+    else if(teks=="162112433045"){
+        var a=1;
+        var anjai = "<p>Kandidat Atas Nama Bintang Putra Narendra "+ teks +" Selamat Anda dinyatakan Lolos pada Divisi Fabrikasi!, Silakan Menunggu Informasi Pelaksanaan Magang</p>";
+        hasil.innerHTML = anjai;
+    }
+    //dandy
+    else if(teks=="162112433087"){
+        var a=1;
+        var anjai = "<p>Kandidat Atas Nama Dandy Satria Wibawa "+ teks +" Selamat Anda dinyatakan Lolos pada Divisi Fabrikasi!, Silakan Menunggu Informasi Pelaksanaan Magang</p>";
+        hasil.innerHTML = anjai;
+    }
+    //dimas
+    else if(teks=="162112433071"){
+        var a=1;
+        var anjai = "<p>Kandidat Atas Nama Dimas Raka Buana Putra "+ teks +" Selamat Anda dinyatakan Lolos pada Divisi Fabrikasi!, Silakan Menunggu Informasi Pelaksanaan Magang</p>";
+        hasil.innerHTML = anjai;
+    }
+    //sean
+    else if(teks=="162112433029"){
+        var a=1;
+        var anjai = "<p>Kandidat Atas Nama Sean Bryant Wijanto "+ teks +" Selamat Anda dinyatakan Lolos pada Divisi Fabrikasi!, Silakan Menunggu Informasi Pelaksanaan Magang</p>";
+        hasil.innerHTML = anjai;
+    }
+    //rikza
+    else if(teks=="162112233056"){
+        var a=1;
+        var anjai = "<p>Kandidat Atas Nama Rikza Maulana "+ teks +" Selamat Anda dinyatakan Lolos pada Divisi Fabrikasi!, Silakan Menunggu Informasi Pelaksanaan Magang</p>";
+        hasil.innerHTML = anjai;
+    }
+    //bhisma
+    else if(teks=="162112233032"){
+        var a=1;
+        var anjai = "<p>Kandidat Atas Nama Bhisma Pradipta Putra "+ teks +" Selamat Anda dinyatakan Lolos pada Divisi Fabrikasi!, Silakan Menunggu Informasi Pelaksanaan Magang</p>";
+        hasil.innerHTML = anjai;
+    }
+    //ade
+    else if(teks=="162112433043"){
+        var a=1;
+        var anjai = "<p>Kandidat Atas Nama Ade Reza "+ teks +" Selamat Anda dinyatakan Lolos pada Divisi Steering & Braking Systems!, Silakan Menunggu Informasi Pelaksanaan Magang</p>";
+        hasil.innerHTML = anjai;
+    }
+    //ficko
+    else if(teks=="166221083"){
+        var a=1;
+        var anjai = "<p>Kandidat Atas Nama Ficko Septarega Putra "+ teks +" Selamat Anda dinyatakan Lolos pada Divisi Steering & Braking Systems!, Silakan Menunggu Informasi Pelaksanaan Magang</p>";
+        hasil.innerHTML = anjai;
+    }
+    //santoso
+    else if(teks=="162112533038"){
+        var a=1;
+        var anjai = "<p>Kandidat Atas Nama Rahmat Santoso Soedirdjo "+ teks +" Selamat Anda dinyatakan Lolos pada Divisi Steering & Braking Systems!, Silakan Menunggu Informasi Pelaksanaan Magang</p>";
+        hasil.innerHTML = anjai;
+    }
+    //asegaf
+    else if(teks=="165221103"){
+        var a=1;
+        var anjai = "<p>Kandidat Atas Nama Asseghaf Bintang Ramadhani "+ teks +" Selamat Anda dinyatakan Lolos pada Divisi Vehicle Design!, Silakan Menunggu Informasi Pelaksanaan Magang</p>";
+        hasil.innerHTML = anjai;
+    }
+    //romeo
+    else if(teks=="165221007"){
+        var a=1;
+        var anjai = "<p>Kandidat Atas Nama Romeo Andromeda Primakusuma Widya Kuntarto "+ teks +" Selamat Anda dinyatakan Lolos pada Divisi Vehicle Design!, Silakan Menunggu Informasi Pelaksanaan Magang</p>";
+        hasil.innerHTML = anjai;
+    }
+    else if(teks==""){
+        var anjing = "<p>Mohon Masukkan NIM Anda dengan Benar!</p>";
+        hasil.innerHTML = anjing;
+    }
+    else{
+        var gada = "<p>Maaf Anda Belum Lolos, Tetap Semangat dan Jangan Menyerah!</p>";
+        hasil.innerHTML = gada;
+    }
+
+    if(a==1){
+        var oke = document.getElementById("hehe");
+        oke.style.display = "block"
+        oke.addEventListener("click", function(){
+            window.location.href = "https://chat.whatsapp.com/B5R0MBfv1XT8nprjajXWXh";
+        });
+    }
+    else{
+        oke.style.display = "none"
+    }
+    
+});
+
+
